e58f2dcc45a6846f2244ffaa1d9aae142a83bbb4
edo2019 <eddyyospher@gmail.com>
2024-04-13 12:21:56 +0300

    proceed

--- lib/screens/loading.dart
+++ lib/screens/loading.dart
@@ -1,4 +1,9 @@
+import 'package:edoblog/constant.dart';
+import 'package:edoblog/models/api_response.dart';
+import 'package:edoblog/services/user_service.dart';
 import 'package:flutter/material.dart';
+import 'package:edoblog/screens/login.dart';
+import 'package:edoblog/screens/home.dart';
 
 class Loading extends StatefulWidget {
   const Loading({super.key});
@@ -8,6 +13,33 @@ class Loading extends StatefulWidget {
 }
 
 class _LoadingState extends State<Loading> {
+  void _loaduserinfo() async {
+    String token = await getToken();
+    if (token == '') {
+      Navigator.of(context).pushAndRemoveUntil(
+          MaterialPageRoute(builder: (context) => Login()), (route) => false);
+    } else {
+      ApiResponse response = await getuserDetail();
+      if (response.error == Null) {
+        Navigator.of(context).pushAndRemoveUntil(
+            MaterialPageRoute(builder: (context) => Home()), (route) => false);
+      } else if (response.error == unauthorized) {
+        Navigator.of(context).pushAndRemoveUntil(
+            MaterialPageRoute(builder: (context) => Home()), (route) => false);
+      } else {
+        ScaffoldMessenger.of(context).showSnackBar(SnackBar(
+          content: Text('${response.error}'),
+        ));
+      }
+    }
+  }
+
+  @override
+  void initState() {
+    _loaduserinfo();
+    super.initState();
+  }
+
   @override
   Widget build(BuildContext context) {
     return Container(
--- lib/services/user_service.dart
+++ lib/services/user_service.dart
@@ -1,4 +1,4 @@
-//login
+import 'package:shared_preferences/shared_preferences.dart';
 
 import 'dart:convert';
 
@@ -7,6 +7,7 @@ import 'package:edoblog/models/user.dart';
 import 'package:edoblog/constant.dart';
 import 'package:http/http.dart' as http;
 
+//login
 Future<ApiResponse> login(String email, String password) async {
   ApiResponse apiResponse = ApiResponse();
   try {
@@ -27,7 +28,95 @@ Future<ApiResponse> login(String email, String password) async {
         apiResponse.error = jsonDecode(response.body)['message'];
         break;
       default:
-        apiResponse.error = serverError;
+        apiResponse.error = somethingwentWrong;
     }
-  } catch (e) {}
+  } catch (e) {
+    apiResponse.error = serverError;
+  }
+  return apiResponse;
+}
+
+//register
+Future<ApiResponse> regisrter(
+    String name, String email, String password) async {
+  ApiResponse apiResponse = ApiResponse();
+  try {
+    var url;
+    final response = await http.post(url.parse(registerURL), headers: {
+      'Accept': 'application/json'
+    }, body: {
+      'name': name,
+      'email': email,
+      'password': password,
+      'password_confirmation': password
+    });
+
+    switch (response.statusCode) {
+      case 200:
+        apiResponse.data = user.fromJson(jsonDecode(response.body));
+        break;
+      case 422:
+        final errors = jsonDecode(response.body)['error'];
+        apiResponse.error = errors[errors.keys.elementsAt(0)][0];
+        break;
+      case 403:
+        apiResponse.error = jsonDecode(response.body)['message'];
+        break;
+      default:
+        apiResponse.error = somethingwentWrong;
+    }
+  } catch (e) {
+    apiResponse.error = serverError;
+  }
+  return apiResponse;
+}
+
+//user
+Future<ApiResponse> getuserDetail(
+    String name, String email, String password) async {
+  ApiResponse apiResponse = ApiResponse();
+  try {
+    String token = await getToken();
+    var url;
+    final response = await http.get(url.parse(userURL), headers: {
+      'Accept': 'application/json',
+      'Authorization': 'Bear $token'
+    });
+
+    switch (response.statusCode) {
+      case 200:
+        apiResponse.data = user.fromJson(jsonDecode(response.body));
+        break;
+      case 422:
+        final errors = jsonDecode(response.body)['error'];
+        apiResponse.error = errors[errors.keys.elementsAt(0)][0];
+        break;
+      case 403:
+        apiResponse.error = jsonDecode(response.body)['message'];
+        break;
+      default:
+        apiResponse.error = somethingwentWrong;
+    }
+  } catch (e) {
+    apiResponse.error = serverError;
+  }
+  return apiResponse;
+}
+
+//get token
+Future<String> getToken() async {
+  SharedPreferences pref = await SharedPreferences.getInstance();
+  return pref.getString('token')??'';
+}
+
+//get user id
+Future<int> getUserId() async {
+  SharedPreferences pref = await SharedPreferences.getInstance();
+  return pref.getInt('userid')?? 0;
+}
+
+//logout
+Future<bool> logout() async {
+  SharedPreferences pref = await SharedPreferences.getInstance();
+  return await pref.remove('token');
 }
